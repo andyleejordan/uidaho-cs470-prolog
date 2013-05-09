58a1b1d7279b6a871686d84420131708c9d52fe5
Andrew Schwartzmeyer <andrew.schwartzmeyer@gmail.com>
2013-05-08 19:46:10 -0700

    Adding parent and gender facts

---
+++ family.pl
@@ -0,0 +1,49 @@
+/* facts */
+
+parent(amy, ginger).
+parent(amy, jackie).
+parent(amy, andrew).
+parent(amy, alex).
+
+parent(jack, ginger).
+parent(jack, jackie).
+parent(jack, andrew).
+parent(jack, alex).
+
+parent(peggy, amy).
+parent(peggy, paige).
+parent(peggy, paula).
+parent(peggy, tim).
+
+parent(woody, amy).
+parent(woody, paige).
+parent(woody, paula).
+parent(woody, tim).
+
+parent(rose, dianne).
+parent(rose, dan).
+parent(rose, jack).
+parent(rose, paul).
+
+parent(george, dianne).
+parent(george, dan).
+parent(george, jack).
+parent(george, paul).
+
+male(jack).
+male(andrew).
+male(alex).
+male(woody).
+male(tim).
+male(george).
+male(dan).
+male(paul).
+
+female(amy).
+female(ginger).
+female(jackie).
+female(peggy).
+female(paige).
+female(paula).
+female(rose).
+female(dianne).
